582ff03579db0eb93c8c4e7e974e70a1e3840ba5
Binatik <sadxas999@mail.ru>
2024-01-03 21:43:50 +0300

    add form control

--- src/module/todo/newTaskForm/NewTaskForm.tsx
+++ src/module/todo/newTaskForm/NewTaskForm.tsx
@@ -1,29 +1,35 @@
 import { Input } from "../../../components/input/Input";
 import { ITask, INewTaskFormProps } from "./newTaskForm.types";
 import { v4 as uuid } from "uuid";
+import { useState } from "react";
 import "./NewTaskForm.css";
 
 function NewTaskForm({ setTodos }: INewTaskFormProps) {
+  const [value, setValue] = useState("");
+
   function handleKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
-    if (event.currentTarget.value.trim() === '') {
+    if (event.currentTarget.value.trim() === "") {
       return;
     }
 
     const todo: ITask = {
       id: uuid(),
-      todoName: event.currentTarget.value,
+      todoName: value,
       create: new Date(),
       isComplited: false,
       status: "none",
-      filter: 'all'
+      filter: "all",
     };
 
     if (event.code === "Enter") {
       setTodos((prev) => [...prev, todo]);
+      setValue("");
     }
   }
 
-  return <Input onKeyDown={handleKeyDown} mode="primary" wide className="new-todo" placeholder="What needs to be done?" />;
+  return (
+    <Input onChange={(event) => setValue(event.currentTarget.value)} value={value} onKeyDown={handleKeyDown} mode="primary" wide className="new-todo" placeholder="What needs to be done?" />
+  );
 }
 
 export { NewTaskForm };
